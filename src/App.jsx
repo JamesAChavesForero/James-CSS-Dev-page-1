1b52dc1703e70cd17efc6e10069f4d9964580038
JamesChaves <james.chaves@sinco.com.co>
2023-08-25 23:18:02 -0500

    wtf??

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './index.css'
 import Header from './components/header/Header';
 import AboutMe from './components/aboutMe/AboutMe';
-import Experience from './components/experience/Experience'
+import Experience from './components/experience/Experience.jsx'
 import Portfolio from './components/portfolio/Portfolio'
 import Referrals from './components/referrals/Referrals'
-import portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/ContactMe'
 function App() {
   return (
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,139 +1,38 @@
 import React from 'react'
 import { BsPatchCheckFill } from 'react-icons/bs'
 import './experience.css'
+const json_data = require('./experience.json');
+const tools = json_data.expObjectArray[0].Tools;
+const skills = json_data.expObjectArray[1].skills
+
+const mimiCardCreator = (arr) => {
+  return arr.map(d => <article className="experience__details flex justify-between items-stretch m-1 p-1 hover:text-white">
+    <BsPatchCheckFill />
+    <h4>{Object.keys(d)[0]}</h4>
+    <small className="text-light">
+      {Object.values(d)[0]} Years
+    </small>
+  </article>
+  )
+}
 
 export default function Experience() {
+
   return (
     <section id='experience'>
-      <h5>what skills I have</h5>
-      <h2>My experience</h2>
-      <div className="container experience__container">
-        <div className="expereince__frontend">
-          <h3>Front End evelopment</h3>
+      <h5>Tools Skills - & - Experience</h5>
+      <div className="container experience__container [&>div]:m-4 [&>div]:text-center">
+        <div className="flex flex-col justify-evenly"id="tools">
+          <h3>Tools Used</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>HTML5</h4>
-              <small className="text-light">
-                Expererience
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>HTML5</h4>
-              <small className="text-light">
-                Advanced
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>CSS3</h4>
-              <small className="text-light">
-                Advanced
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>ECMA6+</h4>
-              <small className="text-light">
-                Advanced
-              </small>
-            </article>
-            {/* <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>React</h4>
-              <small className="text-light">
-                Expererience
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>Tailwind</h4>
-              <small className="text-light">
-                Expererienced
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>Node</h4>
-              <small className="text-light">
-                Intermediate
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>APIs</h4>
-              <small className="text-light">
-                Expererienced
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>JQuery</h4>
-              <small className="text-light">
-                Expererienced
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>Figma UX/UI</h4>
-              <small className="text-light">
-                Intermediate
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>Tailwind</h4>
-              <small className="text-light">
-                Expererience
-              </small>
-            </article> */}
+            {tools && mimiCardCreator(tools)}
           </div>
         </div>
-        <div className="experience__backend">
-          <h3>Back End Development</h3>
-        <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>SQL Server</h4>
-              <small className="text-light">
-                Intermediate
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>FireBsase</h4>
-              <small className="text-light">
-                Expererienced
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>C#</h4>
-              <small className="text-light">
-                Intermediate
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>ASP.Net</h4>
-              <small className="text-light">
-                Intermediate
-              </small>
-            </article>
-             {/*<article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>Java OOP</h4>
-              <small className="text-light">
-                Beginner
-              </small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>Auth2.O</h4>
-              <small className="text-light">
-              Intermediate
-              </small>
-            </article> */}
+        <div className="flex flex-col justify-evenly"id="skills">
+          <h3>Skills Applied</h3>
+          <div className="experience__content">
+            {skills && mimiCardCreator(skills)}
+          </div>
         </div>
       </div>
     </section>
